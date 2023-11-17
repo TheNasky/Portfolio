a12c1c0e978e667b0fb1d45fa22676c6c95eb12b
Valentin Ballesteros <thenasky@gmail.com>
2023-11-17 15:58:19 -0300

    prod 1.1

--- app/components/emailSection.jsx
+++ app/components/emailSection.jsx
@@ -1,3 +1,7 @@
 export default function EmailSection() {
-   return <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">EmailSection</section>;
+   return (
+      <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+         EmailSection
+      </section>
+   );
 }
