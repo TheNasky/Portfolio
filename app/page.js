5a703711904ce753f3338cb33503db44c1029b15
Valentin Ballesteros <thenasky@gmail.com>
2023-11-24 05:23:38 -0300

    metadata 6

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import AboutSection from "./components/aboutSection";
 import AchievementsSection from "./components/achievementSection";
 import AchievementsSection2 from "./components/achievementSection2";
@@ -8,22 +7,9 @@ import Navbar from "./components/navbar";
 import ProjectsSection from "./components/projectsSection";
 import ScrollUpButton from "./components/scrollUpButton";
 
-export const metadata = {
-   title: "Valentín Ballesteros' Portfolio",
-   description: "Valentín Ballesteros' Portfolio",
-   image: "./public/images/icons/NextJs.webp",
-};
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col bg-[#121212]">
-         <Head>
-            <meta name="image" content="./public/images/icons/NextJs.webp" />
-            <meta property="og:title" content="Valentín Ballesteros' Portfolio" />
-            <meta property="og:description" content="Valentín Ballesteros' Portfolio" />
-            <meta property="og:image" content={"./public/images/icons/NextJs.webp"} />
-            {/* Favicon */}
-            <link rel="icon" href="./public/images/icons/NextJs.webp" />
-         </Head>
          <Navbar />
          <div className="container mx-auto mt-16 lg:mt-24 py-8 px-6 lg:px-0">
             <ScrollUpButton />
